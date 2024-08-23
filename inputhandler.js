4ec25cf08ce8a2d582d3f28735295b379ece497d
jbonto <jrbonto1002@gmail.com>
2024-08-23 00:45:18 -0400

    Update inputhandler.js

--- inputhandler.js
+++ inputhandler.js
@@ -2,4 +2,6 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function(addEventListener){
     event.preventDefault();
      console.log("you pressed a button");
+     console.log("this is the test branch");
+     
 })
